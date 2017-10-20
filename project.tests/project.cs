ba115f86e1379dee6c571fd06dda12031c3cea19
alvin niamatali <aniamatali@icloud.com>
2017-10-20 08:23:01 -0700

    tests setup and specs done

---
+++ project.tests/project.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class RepeatCounterTest
+  {
+    [TestMethod]
+    public void CountRepeats_DoesNotAppearInSentence_0()
+    {
+
+    }
+
+    [TestMethod]
+    public void CountRepeats_appearsOnce()
+    {
+
+    }
+
+    [TestMethod]
+    public void CountRepeats_appearsTwice()
+    {
+
+    }
+
+    [TestMethod]
+    public void CountRepeats_Appears_Thrice_withCaps()
+    {
+
+    }
+
+  }
+}
